68b87b0e18e86b39de437ffbd4b25ede06710b78
Srylax <71783705+Srylax@users.noreply.github.com>
2025-04-22 17:01:56 +0200

    feat: Add nix flake

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        server-manifest = (pkgs.lib.importTOML ./yeet-server/Cargo.toml).package;
+        agent-manifest = (pkgs.lib.importTOML ./yeet-agent/Cargo.toml).package;
+      in rec {
+        packages = {
+          yeet-server = pkgs.rustPlatform.buildRustPackage {
+            pname = server-manifest.name;
+            version = server-manifest.version;
+            cargoLock.lockFile = ./Cargo.lock;
+            src = ./.;
+            buildAndTestSubdir = "yeet-server";
+          };
+          yeet-agent = pkgs.rustPlatform.buildRustPackage {
+            pname = agent-manifest.name;
+            version = agent-manifest.version;
+            cargoLock.lockFile = ./Cargo.lock;
+            src = ./.;
+            buildAndTestSubdir = "yeet-agent";
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+            ];
+
+            buildInputs =
+              [pkgs.openssl]
+              ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (
+                with pkgs.darwin.apple_sdk.frameworks; [
+                  SystemConfiguration
+                  CoreServices
+                  Cocoa
+                ]
+              )
+              ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+                pkgs.dbus
+              ];
+          };
+          default = packages.yeet-agent;
+        };
+        nixosModules = import ./module.nix;
+      }
+    );
+}
---
+++ module.nix
@@ -0,0 +1,62 @@
+{
+  yeet-agent = {
+    config,
+    pkgs,
+    lib,
+    ...
+  }:
+    with lib; let
+      cfg = config.services.yeet-agent;
+    in {
+      meta.maintainers = [lib.maintainers.Srylax];
+
+      options.services.yeet-agent = {
+        enable = mkEnableOption "Yeet Deploy Agent: https://github.com/Srylax/yeet";
+
+        url = mkOption {
+          type = types.str;
+          description = "Yeet server url to use.";
+        };
+
+        sleep = mkOption {
+          type = types.int;
+          default = 30;
+          description = "Seconds to wait between updates";
+        };
+
+        package = mkPackageOption pkgs "yeet-agent" {};
+      };
+
+      config = mkIf cfg.enable {
+        systemd.services.yeet-agent = {
+          description = "Yeet Deploy Agent";
+          wants = ["network-online.target"];
+          after = ["network-online.target"];
+          path = [config.nix.package];
+          wantedBy = ["multi-user.target"];
+
+          environment.USER = "root";
+
+          # don't stop the service if the unit disappears
+          unitConfig.X-StopOnRemoval = false;
+
+          serviceConfig = {
+            # we don't want to kill children processes as those are deployments
+            KillMode = "process";
+            Restart = "always";
+            RestartSec = 5;
+            ExecStart = ''
+              ${cfg.package}/bin/yeet-agent  --sleep ${toString cfg.sleep} --url ${cfg.url}
+            '';
+          };
+        };
+      };
+    };
+  # yeet-server = {
+  #   config,
+  #   lib,
+  #   pkgs,
+  #   ...
+  # }: {
+  # };
+}
