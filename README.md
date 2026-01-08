b616807fc03e2f9bd067492b67d316ce755dbf2f
Srylax <71783705+srylax@users.noreply.github.com>
2026-01-08 06:57:42 +0100

    nix: add mdbook devenv

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ![yfwnrm](https://i.kym-cdn.com/photos/images/newsfeed/001/042/619/4ea.jpg)
+
+## Documentation
+
+You can build the latest version of the documentation using `mdook serve book`.
+Make sure you have `mdbook-mermaid` installed and it uses the same version of `mdbook`.
+TODO: add automated github page builds
--- devenv.nix
+++ devenv.nix
@@ -1,19 +1,25 @@
 {
   pkgs,
+  inputs,
   ...
 }:
+let
+  pkgs-unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; };
+in
 {
   languages.rust = {
     enable = false;
   };
 
-  cachix.enable = false;
+  cachix.enable = true;
 
   packages = with pkgs; [
     openssl
-    rustup # because else we cannot use cargo +nightly fmt
+    pkgs-unstable.rustup # because else we cannot use cargo +nightly fmt
     openssl
     gcc
     pkg-config
+    pkgs-unstable.mdbook
+    mdbook-mermaid
   ];
 }
