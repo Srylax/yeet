662d42778e2f5e509387f82a78cdb8255b3f9cc2
Srylax <71783705+srylax@users.noreply.github.com>
2026-01-27 16:28:22 +0100

    yeet: add zlink hash

--- packages/yeet.nix
+++ packages/yeet.nix
@@ -11,7 +11,12 @@ in
 rustPlatform.buildRustPackage {
   pname = manifest.name;
   version = manifest.version;
-  cargoLock.lockFile = ../Cargo.lock;
+  cargoLock = {
+    lockFile = ../Cargo.lock;
+    outputHashes = {
+      "zlink-0.4.0" = "sha256-cS8Oi9zaDcpnP9v12pNSuDEK25KkyC1x55YMcg27qQI=";
+    };
+  };
   src = ../.;
   buildAndTestSubdir = "yeet-agent";
   nativeBuildInputs = [
